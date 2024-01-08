466a4d99834d66fac18bfe15c1b55ad4dbba4244
sathishchinniah04 <87637647+sathishchinniah04@users.noreply.github.com>
2024-01-09 04:48:39 +0530

    chore: initial commit

---
+++ android/src/main/java/com/gamificationjawwylibrary/GamificationJawwyLibraryPackage.kt
@@ -0,0 +1,17 @@
+package com.gamificationjawwylibrary
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class GamificationJawwyLibraryPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(GamificationJawwyLibraryModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  GamificationJawwyLibraryExample
+//
+
+import Foundation
